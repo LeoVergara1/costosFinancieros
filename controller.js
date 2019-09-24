a22f66ee0755584a14e4d8a0ae912612fbdd60cc
Brandon Vergara <brandon@makingdevs.com>
2019-09-24 12:30:53 -0500

    coding master  adding functions to calculate due and amount

--- controller.js
+++ controller.js
@@ -29,7 +29,20 @@ const app = new Vue({
 
   },
   watch: {
-
+    selected(){
+      console.log("Changed watch")
+      this.entry.type= null
+      this.entry.date= null
+      this.entry.concept= null
+      this.entry.income = null
+      this.entry.leave = null
+      this.entry.exist = null
+      this.entry.unity = null
+      this.entry.due = null
+      this.entry.have = null
+      this.entry.amount = null
+      this.entry.medium = null
+    }
   },
   computed: {
     amount(){
@@ -45,7 +58,36 @@ const app = new Vue({
     },
     addEntry(){
       console.log("add")
-      this.listEntries.push(this.entry)
+      if(this.listEntries.length == 0){
+        console.log("Primera vez")
+        this.entry.amount = this.entry.unity * this.entry.income
+        this.entry.due = this.entry.unity * this.entry.income
+        this.entry.exist = this.entry.income
+        this.listEntries.push(Object.assign({}, this.entry))
+      }
+      else {
+        if(this.selected == "factura"){
+          console.log("Factura")
+          let entry = Object.assign({}, this.listEntries[this.listEntries.length - 1]);
+          entry.unity = this.entry.unity
+          entry.exist = Number(entry.exist) + Number(this.entry.income)
+          entry.due = (this.entry.unity * this.entry.income)
+          entry.amount = entry.due + entry.amount
+          entry.type = this.entry.type
+          this.listEntries.push(Object.assign({}, entry))
+        }
+        else{
+          console.log("Vale")
+          let entry = Object.assign({}, this.listEntries[this.listEntries.length - 1]);
+          entry.exist = Number(entry.exist) - Number(this.entry.leave)
+          entry.have = (this.entry.leave * this.entry.medium)
+          entry.leave = this.entry.leave
+          entry.medium = this.entry.medium
+          entry.amount = entry.amount - entry.have
+          entry.type = this.entry.type
+          this.listEntries.push(Object.assign({}, entry))
+        }
+      }
     }
   },
   filters:{
