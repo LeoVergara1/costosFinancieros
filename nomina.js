163e13b20a51c976d01e2e04bacdaed7ec28915f
Brandon Vergara <brandon@makingdevs.com>
2019-09-24 13:53:16 -0500

    coding master  adding new nomimna page

---
+++ nomina.js
@@ -0,0 +1,35 @@
+const app = new Vue({
+  el:"#app",
+  data: {
+    nomina: {
+      salaryMonth: 0,
+      quarter: 0,
+      extraTime: 0,
+      puntuality: 0,
+
+    }
+  },
+  mounted: function() {
+
+  },
+  created: function() {
+
+  },
+  watch: {
+
+  },
+  computed: {
+ 
+  },
+  methods: {
+    calculateNomina(){
+      
+    }
+  },
+  filters:{
+
+  },
+  components: {
+
+  }
+});
